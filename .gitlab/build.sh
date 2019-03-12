bd4b40035d2298d09b37f77b6fd7725357d9267f
Nazmul Alam <nazmulpcc@gmail.com>
2019-03-12 17:12:53 +0600

    fixing ci

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -8,15 +8,18 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 # apt-get install openjdk-8-jdk gcc g++
+apt get install git zip unzip
 
 #install composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php --filename=composer --install-dir=/usr/local/bin
 php -r "unlink('composer-setup.php');"
+
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-7.phar
 chmod +x /usr/local/bin/phpunit
 
+docker-php-ext-install php-zip
 composer install
 

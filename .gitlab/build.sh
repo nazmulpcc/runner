463188d491740238039fd90b7b6166ecab8ba928
Nazmul Alam <nazmulpcc@gmail.com>
2019-03-12 17:05:24 +0600

    test without java

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git openjdk-8-jdk gcc g++ composer -yqq
+apt-get install git gcc g++ composer -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit-7.phar
